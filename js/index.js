bb88c4f9cfbdd781c788cd98280b795519a336ed
RenanSouza <renansouza0122@gmail.com>
2023-02-23 21:52:48 -0300

    underline effect added

--- js/index.js
+++ js/index.js
@@ -5,5 +5,10 @@ tabs.forEach((tab, index)=>{
     tab.addEventListener("click", (e)=>{
         tabs.forEach((tab) => {tab.classList.remove("text-indigo-800")});
         tab.classList.add("text-indigo-800");
+
+        let line = document.querySelector(".line");
+
+        line.style.width = e.target.offsetWidth + 'px';
+        line.style.left = e.target.offsetLeft + 'px'; 
     })
 })
